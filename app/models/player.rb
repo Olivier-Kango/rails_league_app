531c99f0c3696085eb485672c270a69dad1fc349
Olivier-Kango <olivierkango@gmail.com>
2023-02-17 16:56:29 +0200

    Validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,14 @@
 class Player < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true, on: :create
+  validates :age, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
+
+  validate :first_name, :first_name_start_with_vowel
+
+  def first_name_start_with_vowel
+    voyels = ["a", "e", "i", "o", "u"]
+    unless voyels.include? self.first_name[0].downcase
+      errors.add(:first_name, "must start with a vowel")
+    end
+  end
 end
